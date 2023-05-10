6fc769ba9c0fa6ae0d6dbf2a2c41c261b1ad7a9a
cdandor <clintonandor@gmail.com>
2023-05-09 22:07:38 -0700

    updated App.js, App.css and README

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React from "react"
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Zesty</h1>
+        <h2>
+          A photography project <br /> by Ella Fielding
+        </h2>
+      </div>
+      <div className="image-container">
+        <img src="" alt="" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
